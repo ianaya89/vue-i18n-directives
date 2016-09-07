0ca50bb957b4b9fae6f22214bb366437ca58f354
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-09-07 18:14:52 -0300

    Updating exporting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import tDirective  from './t-directive';
 import tpDirective from './tp-directive';
 
-function install(Vue) {
-  if (!Vue) { throw new Error('Missing Vue dependency.'); }
-
-  Vue.use(Vue => {
-    Vue.directive('t',  tDirective);
-    Vue.directive('tp', tpDirective);
-  });
-}
-
-export default install;
+export default Vue => {
+  Vue.directive('t',  tDirective);
+  Vue.directive('tp', tpDirective);
+};
