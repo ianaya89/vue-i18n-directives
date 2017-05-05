510bb1282ed0cbdc7fe8e067546f536603468c13
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-05-05 16:57:56 -0300

    Updating test

--- test/tp-directive.spec.js
+++ test/tp-directive.spec.js
@@ -1,6 +1,6 @@
 /* global describe it expect */
 
-import tpDirective from '../src/t-directive'
+import tpDirective from '../src/tp-directive'
 
 describe('vue-i18n-directive => directive', () => {
   it('has update method available', () => {
