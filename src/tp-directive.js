df3709bc3ea7a13d07963b77641cbe05dfd1ef04
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-05-08 14:03:46 -0300

    Refactoring placeholder and adding bind

--- src/tp-directive.js
+++ src/tp-directive.js
@@ -1,9 +1,12 @@
-export default {
-  update (el, binding, vnode) {
-    const vm = vnode.context
-    if (vm.i18n === false) { return binding.value }
+function translatePlaceholder (el, binding, vnode) {
+  const vm = vnode.context
+  if (vm.i18n === false) { return binding.value }
 
-    const placeholder = vm.$t ? vm.$t(binding.value) : binding.value
-    el.placeholder += placeholder
-  }
+  const placeholder = vm.$t ? vm.$t(binding.value) : binding.value
+  el.placeholder = placeholder
+}
+
+export default {
+  bind: translatePlaceholder,
+  update: translatePlaceholder
 }
