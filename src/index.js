f35a18eb864a949d6b60a5a92276a5bc3c8e2d07
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-05-08 10:48:01 -0300

    Adding translate reset directive

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import translatePlaceholder from './tp-directive'
 import translateCapitalize from './tc-directive'
 import translateUpper from './tu-directive'
 import translateLower from './tl-directive'
+import translateReset from './tr-directive'
 
 export { translate }
 export { translatePlaceholder }
 export { translateCapitalize }
 export { translateUpper }
 export { translateLower }
+export { translateReset }
 
 const plugin = {
   install (Vue) {
@@ -17,6 +19,7 @@ const plugin = {
     Vue.directive('tc', translateCapitalize)
     Vue.directive('tu', translateUpper)
     Vue.directive('tl', translateLower)
+    Vue.directive('tr', translateReset)
   }
 }
 
