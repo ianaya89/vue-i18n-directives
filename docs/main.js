a200b981f19f626d3d930fc8056947fd0f7615ba
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-05-09 18:06:49 -0300

    Improving docs

--- docs/main.js
+++ docs/main.js
@@ -1,28 +1,33 @@
+/* globals Vue VueI18n VueI18nDirectives */
+
 const messages = {
   en: {
-    hi: 'Hi There 🇬🇧'
+    hi: 'hi there 🇬🇧'
   },
 
   es: {
-    hi: 'Hola a Todos 🇪🇸🇦🇷'
+    hi: 'hola a todos 🇪🇸🇦🇷'
   },
 
   fr: {
-    hi: 'Bonjour 🇫🇷'
+    hi: 'bonjour 🇫🇷'
   }
 }
 
 Vue.use(VueI18n)
 Vue.use(VueI18nDirectives)
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+function getParameterByName (name, url) {
+  url = url || window.location.href
+
+  name = name.replace(/[\[\]]/g, '\\$&') // eslint-disable-line
+  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
+  const results = regex.exec(url)
+
+  if (!results) { return null }
+  if (!results[2]) { return '' }
+
+  return decodeURIComponent(results[2].replace(/\+/g, ' '))
 }
 
 const i18n = new VueI18n({
@@ -30,10 +35,16 @@ const i18n = new VueI18n({
   messages
 })
 
-new Vue({
+new Vue({ // eslint-disable-line no-new
   el: '#app',
 
-  directives: { t: VueI18nDirectives.translate },
+  directives: {
+    t: VueI18nDirectives.translate,
+    tl: VueI18nDirectives.translateLower,
+    tu: VueI18nDirectives.translateUpper,
+    tp: VueI18nDirectives.translatePlaceholder,
+    tc: VueI18nDirectives.translateCapitalize
+  },
 
   data () {
     return {
